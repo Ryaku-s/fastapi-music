09e0397222f92615500846ae86f3fe38d324f089
seki <martin.retard@yandex.ru>
2022-05-20 21:22:27 +0300

    Added resend_email_confirmation() route

--- src/app/music/consts.py
+++ src/app/music/consts.py
@@ -9,9 +9,3 @@ class ImageSize(str, Enum):
 class AlbumType(str, Enum):
     ALBUM = 'album'
     SINGLE = 'single'
-
-
-# class AlbumVisibility(str, Enum):
-#     ALL = 'all'
-#     SUBS_ONLY = 'subs'
-#     PRIVATE = 'private'
